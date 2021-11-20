80ecb7bffc3545e766db111116f0181ed6f8cd0c
Kerem Görkem Görgülü <gorkemgorgulu003@gmail.com>
2021-11-21 01:24:57 +0300

    Wall Jump Fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,13 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
 
     public CharacterController controls;
-    public Transform body;
-    public Transform camera;
+    public Transform body, groundCheck, camera;
+    public LayerMask groundMask;
+
     public float speed = 12f;
     public float slidevel = 16f;
     public float gravity = -9.81f;
-    public Transform groundCheck;
-    public LayerMask groundMask;
     public float groundDistance = 0.4f;
-<<<<<<< Updated upstream
-    bool isGrounded;
-    bool slide = false;
-=======
     
     public bool isGrounded, slide = false;
     public bool button_release = false;
@@ -31,13 +26,7 @@ public class PlayerMovement : MonoBehaviour
     bool wall_jump = false;
 
     private float slide_speed, heightinit, current_speed;
->>>>>>> Stashed changes
 
-    Vector3 velocity;
-    float slide_speed;
-    float current_speed;
-    float heightinit;
-    Vector3 camerainit;
     void Start()
     {
         heightinit = controls.height;
@@ -46,8 +35,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-=======
         StayOnGround() ;
 
         velocity.y += gravity * Time.deltaTime;
@@ -65,35 +52,24 @@ public class PlayerMovement : MonoBehaviour
     //Oyuncunun yerde kalmasını ve zıplamasını sağlar
     void StayOnGround()
     {
->>>>>>> Stashed changes
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded == true && velocity.y < 0)
+
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -4f;
             velocity.x = 0;
             velocity.z = 0;
+            current_wall = null;
+            prev_wall = null;
         }
-
+        
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = 5.4f;
+            speed = 6f;
         }
+    }
 
-<<<<<<< Updated upstream
-        velocity.y += gravity * Time.deltaTime;
-        controls.Move(velocity * Time.deltaTime);
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        controls.Move(move * speed * Time.deltaTime);
-
-        slide_speed = Mathf.Sqrt(Mathf.Pow(12f,2f) + Mathf.Pow(0.12f,2f));
-        current_speed = Mathf.Sqrt(Mathf.Pow(controls.velocity.x, 2f) + Mathf.Pow(controls.velocity.z, 2f));
-
-        if (Input.GetKeyDown(KeyCode.LeftControl) && current_speed > slide_speed / 1.5)
-=======
     //Oyuncunun kaymasını sağlar
     void isSliding(Vector3 move, float slide_speed, float current_speed)
     {
@@ -104,11 +80,6 @@ public class PlayerMovement : MonoBehaviour
             StartCoroutine(CooldownTimer());
         }
         if (slide)
->>>>>>> Stashed changes
-        {
-            slide = true;
-        }
-        if(slide)
         {
             controls.height = heightinit / 4;
             camera.localPosition = new Vector3(camerainit.x, camerainit.y / 3, camerainit.z);
@@ -124,8 +95,6 @@ public class PlayerMovement : MonoBehaviour
             }
         }
     }
-<<<<<<< Updated upstream
-=======
 
     private Vector3 move()
     {
@@ -161,9 +130,8 @@ public class PlayerMovement : MonoBehaviour
         }
         if (wall_jump == false)
         {
-            velocity.x = hit.normal.x * 12;
-            velocity.z = hit.normal.z * 12;
+            velocity.x = hit.normal.x * 11;
+            velocity.z = hit.normal.z * 11;
         }
     }
->>>>>>> Stashed changes
 }
